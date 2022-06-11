0e36e7a106c881eed03c33f805c822465978e37f
Maksym Bilan <>
2022-06-11 14:50:16 +0300

    Project template

---
+++ Patchcord/PatchcordApp.swift
@@ -0,0 +1,20 @@
+//
+//  PatchcordApp.swift
+//  Patchcord
+//
+//  Created by Maksym Bilan on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PatchcordApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
