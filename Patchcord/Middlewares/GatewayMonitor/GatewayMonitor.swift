d0e549aeebbb95c9a2b79f4fdc79c6a411535eb7
Maksym Bilan <>
2022-06-25 18:05:47 +0300

    Netmask

--- Patchcord/Middlewares/GatewayMonitor/GatewayMonitor.swift
+++ Patchcord/Middlewares/GatewayMonitor/GatewayMonitor.swift
@@ -28,6 +28,7 @@ final class GatewayMonitor {
         if let ipAddress = UIDevice.current.getIP() {
             print(ipAddress)
         }
+        print(getIfaNetmask())
     }
 
 }
@@ -83,4 +84,48 @@ extension UIDevice {
         return address
     }
 
+}
+
+func getIfaNetmask() -> String {
+    var ifaNetmask = ""
+    //Get list of all interfaces on the local machine:
+    var ifaddr : UnsafeMutablePointer<ifaddrs>? = nil
+    if getifaddrs(&ifaddr) == 0 {
+
+        var ptr = ifaddr;
+        while ptr != nil {
+            let name = String.init(utf8String: ptr!.pointee.ifa_name)  //[NSString stringWithUTF8String:ptr->ifa_name];
+            if (name == "en0")
+            {
+            let flags = Int32((ptr?.pointee.ifa_flags)!)
+            var addr = ptr?.pointee.ifa_addr.pointee
+
+            //Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+            if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                if addr?.sa_family == UInt8(AF_INET) || addr?.sa_family == UInt8(AF_INET6) {
+
+                    //Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    if (getnameinfo(&addr!, socklen_t((addr?.sa_len)!), &hostname, socklen_t(hostname.count),
+                                    nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                        if let address = String.init(validatingUTF8:hostname) {
+
+                            var net = ptr?.pointee.ifa_netmask.pointee
+                            var netmaskName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                            getnameinfo(&net!, socklen_t((net?.sa_len)!), &netmaskName, socklen_t(netmaskName.count),
+                                        nil, socklen_t(0), NI_NUMERICHOST)//== 0
+                            if let netmask = String.init(validatingUTF8:netmaskName) {
+                                print("address=/(address),netmask\(netmask)")
+                                ifaNetmask = netmask
+                            }
+                        }
+                    }
+                }
+            }
+            }
+            ptr = ptr?.pointee.ifa_next
+        }
+        freeifaddrs(ifaddr)
+    }
+    return ifaNetmask
 }
